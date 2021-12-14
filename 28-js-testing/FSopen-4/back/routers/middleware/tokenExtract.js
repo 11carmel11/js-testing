767812f7affda7be7cd011dde90360d1977e64b2
11carmel11 <11carmrl11@gmail.com>
2021-12-14 15:32:50 +0200

    Update tokenExtract.js

--- 28-js-testing/FSopen-4/back/routers/middleware/tokenExtract.js
+++ 28-js-testing/FSopen-4/back/routers/middleware/tokenExtract.js
@@ -1,6 +1,6 @@
 module.exports = (req, _res, next) => {
-  const { authentication } = req.headers;
-  const token = authentication?.split(" ")[1];
+  const { authorization } = req.headers;
+  const token = authorization?.split(" ")[1];
   req.token = token || "";
   next();
 };
