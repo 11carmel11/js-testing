c19f1d5f64f71292acd44a109b320d11a2a29177
11carmel11 <11carmrl11@gmail.com>
2021-12-16 17:23:41 +0200

    5.18-logging in tests

--- 28-js-testing/FSopen-5/front/cypress/integration/1-getting-started/login.test.js
+++ 28-js-testing/FSopen-5/front/cypress/integration/1-getting-started/login.test.js
@@ -1,10 +1,37 @@
-describe("basic e2e", () => {
+const mock = {
+  username: "testUser",
+  wrongUsername: "wrongUsername",
+  password: "testPassword",
+  wrongPassword: "wrongPassword",
+  name: "test test",
+};
+
+describe("login", function () {
   beforeEach(() => {
     cy.request("PUT", "http://localhost:3003/api/reset");
+    cy.request({
+      url: "http://localhost:3003/api/users",
+      method: "POST",
+      body: mock,
+    });
+
     cy.visit("http://localhost:3000");
   });
 
-  it("should get to login-page by default", () => {
+  it("should get to login-page by default", function () {
     cy.contains("login here");
+  });
+
+  describe("logging in", function () {
+    it("should let a valid user log in", function () {
+      cy.login(mock.username, mock.password);
+
+      cy.contains("blogs:");
+    });
+
+    it("should not let an invalid user log in", function () {
+      cy.login(mock.username, mock.wrongPassword);
+      cy.get(".notyf__toast--error");
+    });
   });
 });
--- 28-js-testing/FSopen-5/front/cypress/support/commands.js
+++ 28-js-testing/FSopen-5/front/cypress/support/commands.js
@@ -10,7 +10,11 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("login", (username, password) => {
+  cy.get("#username").type(username);
+  cy.get("#password").type(password);
+  cy.contains("login!").click();
+});
 //
 //
 // -- This is a child command --
