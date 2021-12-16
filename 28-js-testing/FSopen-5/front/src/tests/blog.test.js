caf4320721ac939f83a8826e6e5cf8dd11da2def
11carmel11 <11carmrl11@gmail.com>
2021-12-16 12:07:32 +0200

    5.15-test like btn

--- 28-js-testing/FSopen-5/front/src/tests/blog.test.js
+++ 28-js-testing/FSopen-5/front/src/tests/blog.test.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import {
-  render,
-  fireEvent,
-  waitFor,
-  findByTestId,
-} from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Blog from "../components/Blog";
 
 const mock = {
@@ -31,30 +26,47 @@ const mock = {
 };
 
 describe("<Blog/> Component", () => {
-  let component;
-
-  beforeEach(() => {
-    component = render(
-      <Blog
-        blog={mock.fullBlog}
-        username={mock.fullBlog.author}
-        token={mock.token}
-      />
-    );
-  });
+  //#region setup
+  //#endregion
 
   it("should render generic blog", () => {
+    //#region setup
+    const mockLiker = jest.fn();
+    const mockRemover = jest.fn();
+    const component = render(
+      <Blog
+        blog={mock.fullBlog}
+        username={mock.user.username}
+        likeHandler={mockLiker}
+        remover={mockRemover}
+      />
+    );
+    //#endregion
+
     const title = component.getByText(`title: ${mock.fullBlog.title}`);
     const author = component.getByText(`author: ${mock.fullBlog.author}`);
     const viewBtn = component.getByText("view more");
-    const url = component.container.querySelectorAll(".url");
+    const url = component.container.querySelector(".url");
     expect(author).toBeDefined();
     expect(viewBtn).toBeDefined();
     expect(title).toBeDefined();
-    expect(url).toHaveLength(0);
+    expect(url).toEqual(null);
   });
 
-  it("should show all details onClick on button", () => {
+  it("should show all details when clicking on `view more`", () => {
+    //#region setup
+    const mockLiker = jest.fn();
+    const mockRemover = jest.fn();
+    const component = render(
+      <Blog
+        blog={mock.fullBlog}
+        username={mock.user.username}
+        likeHandler={mockLiker}
+        remover={mockRemover}
+      />
+    );
+    //#endregion
+
     const viewBtn = component.container.querySelector(".view-btn");
     expect(viewBtn).toBeDefined();
 
@@ -64,29 +76,32 @@ describe("<Blog/> Component", () => {
     expect(url).toHaveLength(1);
   });
 
-  it("should add like when clicking", async () => {
+  it("should add like when clicking", () => {
+    //#region setup
+    const mockLiker = jest.fn();
+    const mockRemover = jest.fn();
+    const component = render(
+      <Blog
+        blog={mock.fullBlog}
+        username={mock.user.username}
+        likeHandler={mockLiker}
+        remover={mockRemover}
+      />
+    );
+    //#endregion
+
     const viewBtn = component.container.querySelector(".view-btn");
+    expect(viewBtn).not.toEqual(null);
+
     fireEvent.click(viewBtn);
-    // let likesElm = component.container.querySelector(".likes");
 
     const likeBtn = component.container.querySelector(".like-btn");
-    fireEvent.click(likeBtn);
+    expect(likeBtn).not.toEqual(null);
 
-    await findByTestId();
+    for (let i = 1; i < 5; i++) {
+      fireEvent.click(likeBtn);
 
-    await waitFor(() => {
-      expect(
-        document.querySelectorAll(".notyf__toast--dismissible")
-      ).toHaveLength(1);
-    });
-
-    // await waitForElementToBeRemoved(() => {
-    //   expect(likesElm).toHaveTextContent("likes: 101 👍");
-    // }, {interval:});
-    // likesElm = component.container.querySelector(".likes");
-
-    expect(component.container.querySelector(".likes")).toHaveTextContent(
-      "likes: 101 👍"
-    );
+      expect(mockLiker.mock.calls).toHaveLength(i);
+    }
   });
 });
